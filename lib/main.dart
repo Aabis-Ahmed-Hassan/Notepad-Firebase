cb408aaf34f1a800fe5508468364d7663d4b1bbc
Aabis Ahmed Hassan <aabisahmedhassan3@gmail.com>
2024-04-06 07:42:17 +0500

    Done with Basics for Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notepad_with_firebase/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -9,6 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Text(
+        'Hello World',
+      ),
+    );
   }
 }
