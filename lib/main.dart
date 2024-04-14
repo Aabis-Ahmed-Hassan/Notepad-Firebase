1847e60e066d6577723142ded1bec9f8d06a330d
Aabis Ahmed Hassan <aabisahmedhassan3@gmail.com>
2024-04-14 07:54:17 +0500

    Minor Changes

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => SignupProvider()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: SplashScreen(),
       ),
     );
